40ac606fa98e537f031f64c58e49614990778088
bw-CFD-VT <79177590+bw-CFD-VT@users.noreply.github.com>
2021-03-18 21:29:48 -0400

    Update Comment(s)

--- VariableSwap.cpp
+++ VariableSwap.cpp
@@ -29,10 +29,10 @@ void conserved_to_primitive (vector<double> U, vector<double> &V)
     V[2] = (gam-1)*(U[2] - U[0]*(((U[1]*U[1])/(U[0]*U[0]))/2)); 
 
 
-    //------------ Limit Primative Variables ----------//
+    //------------ Limit Primitive Variables ----------//
     vector<double> Primitive_Limits(3,0);
     
-    Primitive_Limits[0] = 0.0001; Primitive_Limits[1] = 10.0; Primitive_Limits[2] = 500; //Nozzle Shock
+    Primitive_Limits[0] = 0.0001; Primitive_Limits[1] = 10.0; Primitive_Limits[2] = 500; 
     
     for (int j = 0; j<3; j++) V[j] = max(V[j],Primitive_Limits[j]);
 
